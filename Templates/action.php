86bc8eaf7a962f1367eef9b6e19d0fa0efd84f94
ivanfomin <la-verne@mail.ru>
2016-12-15 08:29:47 +0300

    authorization_1

--- Templates/action.php
+++ Templates/action.php
@@ -3,8 +3,8 @@
 require_once __DIR__ . '/../autoloads.php';
 
 $id = $_POST['id'];
-$title = $_POST['title'];
-$content = $_POST['content'];
+$title = htmlspecialchars($_POST['title']);
+$content = htmlspecialchars($_POST['content']);
 $user_id = $_POST['user_id'];
 
 if (isset($_POST['update'])) {
@@ -12,7 +12,7 @@ if (isset($_POST['update'])) {
     $article->title = $title;
     $article->content = $content;
     $article->save();
-} else if (isset($_POST['insert'])) {
+} else if (isset($_POST['insert']) && !empty($title)) {
     $article = new Model\Article();
     $article->title = $title;
     $article->content = $content;
